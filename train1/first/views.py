3982cf61e7a5bcbc831863b23559d7012d6212c6
yekai2233 <1184463199@qq.com>
2018-04-20 07:39:30 +0800

    2018.4.20

--- train1/first/views.py
+++ train1/first/views.py
@@ -1,3 +1,23 @@
+#coding:utf-8
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+
+def index(request):
+	return render(request, 'first/home.html')
+
+def add(request):
+	a = request.GET['a']
+	b = request.GET['b']
+	c = int(a) + int(b)
+	return HttpResponse(str(c))
+
+def add2(request, a, b):
+	
+	c = int(a) + int(b)
+	return HttpResponse(str(c))
